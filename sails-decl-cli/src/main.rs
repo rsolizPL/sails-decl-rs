bdf00e1447f48bc40c7240fa3548b018841e2202
Rudy Soliz <rsoliz@propulsionlabs.com>
2026-02-08 01:03:39 -0600

    works with models

--- sails-decl-cli/src/main.rs
+++ sails-decl-cli/src/main.rs
@@ -1,13 +1,13 @@
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
-use swc_ecma_codegen::{to_code};
+use swc_ecma_codegen::to_code;
 
 #[derive(Parser)]
 #[command(
     version = "0.1.0",
     author = "Rubyboat <me@rubyboat.net>",
-    about = "A CLI tool to add type declarations to Sails.js projects",
+    about = "A CLI tool to add type declarations to Sails.js projects"
 )]
 struct Cli {
     #[command(subcommand)]
@@ -17,32 +17,102 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     #[command(name = "run")]
-    Run{
-        #[arg(short = 'p', long = "project-root", value_parser)]
+    Run {
+        #[arg(value_parser)]
         project_root: Option<PathBuf>,
         #[arg(short = 'i', long = "ignored-files", value_parser)]
         ignored_files: Vec<PathBuf>,
+        #[arg(short = 'm', long = "model-dir", value_parser)]
+        model_dir: Option<PathBuf>,
+        #[arg(short = 'e', long = "helpers-dir", value_parser)]
+        helpers_dir: Option<PathBuf>,
+        #[arg(short = 't', long = "types-dir", value_parser)]
+        types_dir: Option<PathBuf>,
     },
 }
 
 fn main() {
-    let model_file = r#"
-    module.exports = {
-        attributes: {
-            name: { type: 'string', required: true },
-            age: { type: 'number' },
-            isActive: { type: 'boolean', defaultsTo: true },
-            tags: {
-                type: 'json',
-                "$SD-type-hint": "{ color: string, label: string }[]",
-            },
-            profile: { model: 'profile' },
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Run {
+            project_root,
+            ignored_files,
+            model_dir,
+            helpers_dir,
+            types_dir
+        }) => run(
+            project_root,
+            ignored_files,
+            model_dir,
+            helpers_dir,
+            types_dir
+        ),
+        None => run(&None, &vec![], &None, &None, &None),
+    }
+}
+
+fn run(
+    project_root: &Option<PathBuf>,
+    ignored_files: &Vec<PathBuf>,
+    model_dir: &Option<PathBuf>,
+    helpers_dir: &Option<PathBuf>,
+    types_dir: &Option<PathBuf>,
+) {
+    let cwd = std::env::current_dir().expect("Failed to get current directory");
+    let project_root = project_root.as_ref().unwrap_or(&cwd);
+
+    // Check for .sailsrc file in project_root
+    let sailsrc_path = project_root.join(".sailsrc");
+    if !sailsrc_path.exists() {
+        eprintln!("Error: .sailsrc file not found in project root: {}", project_root.display());
+        std::process::exit(1);
+    }
+
+    let model_dir = model_dir
+        .as_ref()
+        .unwrap_or(&project_root.join("api/models"))
+        .clone();
+    let _helpers_dir = helpers_dir
+        .as_ref()
+        .unwrap_or(&project_root.join("api/helpers"))
+        .clone();
+    let types_dir = types_dir
+        .as_ref()
+        .unwrap_or(&project_root.join("typings"))
+        .clone();
+    let models_types_dir = types_dir.join("models");
+
+    // recursively find all .js files in the model_dir, excluding ignored_files
+    let js_files = glob::glob(&format!("{}/**/*.js", model_dir.display()))
+        .expect("Failed to read glob pattern")
+        .filter_map(Result::ok)
+        .filter(|path| {
+            !ignored_files
+                .iter()
+                .any(|ignored| path.starts_with(ignored))
+        })
+        .collect::<Vec<_>>();
+
+    println!(
+        "Found {} Models in {}",
+        js_files.len(),
+        model_dir.display()
+    );
+
+    for js_file in js_files {
+        let code = std::fs::read_to_string(&js_file).expect("Failed to read model file");
+        let name = js_file.file_stem().unwrap().to_string_lossy().to_string();
+        match sails_decl_core::gen_decl_for_model(code, name) {
+            Ok(decl) => {
+                let decl_code = to_code(&decl);
+                // same relative path as the js file, but with .d.ts extension and in the types_dir
+                let new_path = models_types_dir.join(js_file.strip_prefix(&model_dir).unwrap()).with_extension("d.ts");
+                std::fs::create_dir_all(new_path.parent().unwrap()).expect("Failed to create directories for output file");
+                std::fs::write(&new_path, decl_code).expect("Failed to write declaration file");
+                println!("Generated declaration for {} at {}", js_file.display(), new_path.display());
+            }
+            Err(e) => eprintln!("Error processing {}: {:?}", js_file.display(), e),
         }
-    };"#.to_string();
-
-    let model_name = "User".to_string();
-
-    let decl = sails_decl_core::gen_decl_for_model(model_file, model_name).unwrap();
-
-    println!("Generated Declaration: {}", to_code(&decl));
+    }
 }
--- sails-decl-core/src/lib.rs
+++ sails-decl-core/src/lib.rs
@@ -3,26 +3,31 @@ extern crate swc_ecma_parser;
 
 use swc_common::sync::Lrc;
 use swc_common::{
-    errors::{ColorConfig, Handler},
     FileName, SourceMap,
+    errors::{ColorConfig, Handler},
 };
-use swc_ecma_parser::{lexer::Lexer, Parser, StringInput, Syntax};
+use swc_ecma_parser::{Parser, StringInput, Syntax, lexer::Lexer};
+use swc_ecmascript::ast::TsInterfaceDecl;
 use swc_ecmascript::ast::TsType::{self};
-use swc_ecmascript::ast::{Expr, Ident, Lit, PropName, Str, TsInterfaceBody, TsKeywordType, TsKeywordTypeKind, TsPropertySignature, TsTypeAnn, TsTypeElement};
-use swc_ecmascript::{
-    ast::{TsInterfaceDecl},
+use swc_ecmascript::ast::{
+    Expr, Ident, Lit, PropName, Str, TsInterfaceBody, TsKeywordType, TsKeywordTypeKind,
+    TsPropertySignature, TsTypeAnn, TsTypeElement,
 };
 #[derive(Debug)]
 pub enum GenDeclForModelError {
     ParseError,
     InvalidModel,
+    IsNotCommonJsModule,
+    CommonJsModuleDoesNotExportObject,
+    SDTypeHintParseError,
 }
 
-pub fn gen_decl_for_model(code: String, model_name: String) -> Result<TsInterfaceDecl, GenDeclForModelError> {
+pub fn gen_decl_for_model(
+    code: String,
+    model_name: String,
+) -> Result<TsInterfaceDecl, GenDeclForModelError> {
     let cm: Lrc<SourceMap> = Default::default();
-    let handler =
-        Handler::with_tty_emitter(ColorConfig::Auto, true, false,
-        Some(cm.clone()));
+    let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
 
     let file = cm.new_source_file(FileName::Anon.into(), code);
 
@@ -30,7 +35,7 @@ pub fn gen_decl_for_model(code: String, model_name: String) -> Result<TsInterfac
         Syntax::Es(Default::default()),
         Default::default(),
         StringInput::from(&*file),
-        None
+        None,
     );
 
     let mut parser = Parser::new_from(lexer);
@@ -39,280 +44,163 @@ pub fn gen_decl_for_model(code: String, model_name: String) -> Result<TsInterfac
         e.into_diagnostic(&handler).emit();
     }
 
-    let _module_res = parser.parse_commonjs().map_err(|e| {
-            // Unrecoverable fatal error occurred
-            e.into_diagnostic(&handler).emit()
-        });
-    
-    if _module_res.is_err() {
+    let module_result = parser.parse_commonjs().map_err(|e| {
+        // Unrecoverable fatal error occurred
+        e.into_diagnostic(&handler).emit()
+    });
+
+    if module_result.is_err() {
         return Err(GenDeclForModelError::ParseError);
     }
 
-    let _module = _module_res.unwrap();
+    let module = module_result.unwrap();
 
     let mut elements: Vec<TsTypeElement> = vec![];
 
-    let mut _found_valid_model = false;
-
-    // look for module.exports, then attributes
-    for _item in _module.body {
-        if !_item.is_expr() {
-            continue;
-        }
+    let module_exports_obj = module.body.iter().find_map(|item| {
+        let assign = item.as_expr()?.expr.as_assign()?;
+        let member = assign.left.as_simple()?.as_member()?;
         
-        let _expression = _item.as_expr().unwrap();
-
-        if !_expression.expr.is_assign() {
-            continue;
+        if member.obj.as_ident()?.sym != "module" || member.prop.as_ident()?.sym != "exports" {
+            return None;
         }
 
-        let _assignment = _expression.expr.as_assign().unwrap();
+        assign.right.as_object().cloned()
+    }).ok_or(GenDeclForModelError::IsNotCommonJsModule)?;
 
-        if !_assignment.left.is_simple() {
-            continue;
+    let attributes_obj = module_exports_obj.props.iter().find_map(|prop| {
+        let _key_value_prop = prop.as_prop().and_then(|p| p.as_key_value())?;
+        let key_name = get_prop_as_str(&_key_value_prop.key)?;
+
+        if key_name != "attributes" {
+            return None;
         }
 
-        let _simple_left = _assignment.left.as_simple().unwrap();
+        _key_value_prop.value.as_object().cloned()
+    }).ok_or(GenDeclForModelError::InvalidModel)?;
 
-        if !_simple_left.is_member() {
-            continue;
-        }
+    for attribute in &attributes_obj.props {
+        let attribute_pair = match attribute.as_prop().and_then(|p| p.as_key_value()) {
+            Some(prop) => prop,
+            None => continue,
+        };
 
-        let _member_expr = _simple_left.as_member().unwrap();
+        let _attr_key_ident = match attribute_pair.key.as_ident() {
+            Some(ident) => ident,
+            None => continue,
+        };
 
-        if !_member_expr.obj.is_ident() {
-            continue;
-        }
+        let _attr_value_obj = match attribute_pair.value.as_object() {
+            Some(obj) => obj,
+            None => continue,
+        };
 
-        let _obj_ident = _member_expr.obj.as_ident().unwrap();
+        let mut attribute_type: Option<&str> = None;
+        let mut attribute_type_hint: Option<&str> = None;
+        let mut attribute_required: bool = false;
 
-        if _obj_ident.sym != *"module" {
-            continue;
-        }
+        for _attr_field in &_attr_value_obj.props {
+            let _attr_field_prop = match _attr_field.as_prop() {
+                Some(prop) => prop,
+                None => continue,
+            };
 
-        if !_member_expr.prop.is_ident() {
-            continue;
-        }
+            let attribute_pair = _attr_field_prop.as_key_value().unwrap();
 
-        let _prop_ident = _member_expr.prop.as_ident().unwrap();
+            let attribute_name = match get_prop_as_str(&attribute_pair.key) {
+                Some(name) => name,
+                None => continue,
+            };
+            let attribute_value = &attribute_pair.value;
 
-        if _prop_ident.sym != *"exports" {
-            continue;
-        }
-
-        _found_valid_model = true;
-
-        // Now look for attributes inside the right hand side
-        if !_assignment.right.is_object() {
-            continue;
-        }
-
-        let _obj_lit = _assignment.right.as_object().unwrap();
-
-        for _prop in &_obj_lit.props {
-            // look for attributes property
-            if !_prop.is_prop() {
-                continue;
-            }
-
-            let _key_value = _prop.as_prop().unwrap();
-
-            if !_key_value.is_key_value() {
-                continue;
-            }
-
-            let _key_value_prop = _key_value.as_key_value().unwrap();
-
-            if !_key_value_prop.key.is_ident() {
-                continue;
-            }
-
-            let _key_ident = _key_value_prop.key.as_ident().unwrap();
-
-            if _key_ident.sym != *"attributes" {
-                continue;
-            }
-
-            // We found attributes, now we can process the properties
-            if !_key_value_prop.value.is_object() {
-                continue;
-            }
-
-            let _attributes_obj = _key_value_prop.value.as_object().unwrap();
-
-            for _attribute_prop in &_attributes_obj.props {
-                // Process each attribute property here
-                // For simplicity, we will skip the actual processing logic
-                // and just print the attribute names
-
-                if !_attribute_prop.is_prop() {
-                    continue;
-                }
-
-                let _attr_key_value = _attribute_prop.as_prop().unwrap();
-
-                if !_attr_key_value.is_key_value() {
-                    continue;
-                }
-
-                let _attr_key_value_prop = _attr_key_value.as_key_value().unwrap();
-
-                if !_attr_key_value_prop.key.is_ident() {
-                    continue;
-                }
-
-                let _attr_key_ident = _attr_key_value_prop.key.as_ident().unwrap();
-
-
-                if !_attr_key_value_prop.value.is_object() {
-                    continue;
-                }
-
-                let _attr_value_obj = _attr_key_value_prop.value.as_object().unwrap();
-
-                let mut _type_prop_value: Option<&str> = None;
-                let mut _type_hint_prop_value: Option<&str> = None;
-                let mut _required_prop_value: bool = false;
-                
-                for _attr_field in &_attr_value_obj.props {
-                    if !_attr_field.is_prop() {
-                        continue;
-                    }
-
-                    let _attr_field_prop = _attr_field.as_prop().unwrap();
-                    if !_attr_field_prop.is_key_value() {
-                        continue;
-                    }
-
-                    let _attr_field_key_value = _attr_field_prop.as_key_value().unwrap();
-
-                    let _attr_field_key_ident = match &_attr_field_key_value.key {
-                        PropName::Ident(ident) => ident.sym.as_str(),
-                        PropName::Str(string) => string.value.as_str().unwrap(),
-                        _ => continue,
-                    };
-
-                    match _attr_field_key_ident {
-                        "type" => {
-                            if _attr_field_key_value.value.is_lit() {
-                                let _lit = _attr_field_key_value.value.as_lit().unwrap();
-                                if _lit.is_str() {
-                                    _type_prop_value = _lit.as_str().unwrap().value.as_str();
-                                }
-                            }
-                        }
-                        "required" => {
-                            if _attr_field_key_value.value.is_lit() {
-                                let _lit = _attr_field_key_value.value.as_lit().unwrap();
-                                if _lit.is_bool() {
-                                    _required_prop_value = _lit.as_bool().unwrap().value;
-                                }
-                            }
-                        }
-                        "$SD-type-hint" => {
-                            println!("Found $SD-type-hint");
-                            if _attr_field_key_value.value.is_lit() {
-                                let _lit = _attr_field_key_value.value.as_lit().unwrap();
-                                if _lit.is_str() {
-                                    _type_hint_prop_value = _lit.as_str().unwrap().value.as_str();
-                                }
-                            }
-                        }
-                        _ => {
-                            continue;
+            match attribute_name {
+                "type" => {
+                    if attribute_value.is_lit() {
+                        let _lit = attribute_value.as_lit().unwrap();
+                        if _lit.is_str() {
+                            attribute_type = _lit.as_str().unwrap().value.as_str();
                         }
                     }
                 }
-
-                let type_annotation: Result<TsTypeAnn, ()> = match _type_hint_prop_value.or(_type_prop_value) {
-                    Some("string") => Ok(TsTypeAnn {
-                        span: Default::default(),
-                        type_ann: Box::new(TsType::TsKeywordType(TsKeywordType {
-                            span: Default::default(),
-                            kind: TsKeywordTypeKind::TsStringKeyword,
-                        })),
-                    }),
-                    Some("number") => Ok(TsTypeAnn {
-                        span: Default::default(),
-                        type_ann: Box::new(TsType::TsKeywordType(TsKeywordType {
-                            span: Default::default(),
-                            kind: TsKeywordTypeKind::TsNumberKeyword,
-                        })),
-                    }),
-                    Some("boolean") => Ok(TsTypeAnn {
-                        span: Default::default(),
-                        type_ann: Box::new(TsType::TsKeywordType(TsKeywordType {
-                            span: Default::default(),
-                            kind: TsKeywordTypeKind::TsBooleanKeyword,
-                        })),
-                    }),
-                    Some("json") => Ok(TsTypeAnn {
-                        span: Default::default(),
-                        type_ann: Box::new(TsType::TsKeywordType(TsKeywordType {
-                            span: Default::default(),
-                            kind: TsKeywordTypeKind::TsAnyKeyword,
-                        })),
-                    }),
-                    Some(x) => {
-                        println!("Trying to parse type hint: {}", x);
-                        // try parsing it, and see if you can make a type out of it
-                        let temp_type = format!("0 as {}", x);
-                        let type_file = cm.new_source_file(FileName::Anon.into(), temp_type);
-
-                        let type_lexer = Lexer::new(
-                            Syntax::Typescript(Default::default()),
-                            Default::default(),
-                            StringInput::from(&*type_file),
-                            None
-                        );
-
-                        let mut type_parser = Parser::new_from(type_lexer);
-
-                        if let Ok(expr) = type_parser.parse_expr() {
-                            if let Some(_ts_as) = expr.as_ts_as() {
-                                Ok(TsTypeAnn {
-                                    span: Default::default(),
-                                    type_ann: _ts_as.type_ann.clone(),
-                                })
-                            }else {
-                                Err(())
-                            }
-                        }else {
-                            Err(())
+                "required" => {
+                    if attribute_value.is_lit() {
+                        let _lit = attribute_value.as_lit().unwrap();
+                        if _lit.is_bool() {
+                            attribute_required = _lit.as_bool().unwrap().value;
                         }
                     }
-                    None => continue,
-                };
-
-                if _type_prop_value.is_some() {
-                    elements.push(TsTypeElement::TsPropertySignature(TsPropertySignature{
-                        span: Default::default(),
-                        readonly: true,
-                        key: Box::new(
-                            Expr::Lit(Lit::Str(Str{
-                                span: Default::default(),
-                                value: _attr_key_ident.sym.as_str().into(),
-                                raw: None 
-                            }))
-                        ),
-                        computed: true,
-                        optional: !_required_prop_value,
-                        type_ann: Some(Box::new(type_annotation.unwrap())),
-                    }));
                 }
-
-                
+                "$SD-type-hint" => {
+                    if attribute_value.is_lit() {
+                        let _lit = attribute_value.as_lit().unwrap();
+                        if _lit.is_str() {
+                            attribute_type_hint = _lit.as_str().unwrap().value.as_str();
+                        }
+                    }
+                }
+                _ => {
+                    continue;
+                }
             }
+        }
 
+        let type_annotation: Result<TsTypeAnn, ()> = match attribute_type_hint.or(attribute_type) {
+            Some("string") => Ok(TsTypeAnn {
+                span: Default::default(),
+                type_ann: Box::new(TsType::TsKeywordType(TsKeywordType {
+                    span: Default::default(),
+                    kind: TsKeywordTypeKind::TsStringKeyword,
+                })),
+            }),
+            Some("number") => Ok(TsTypeAnn {
+                span: Default::default(),
+                type_ann: Box::new(TsType::TsKeywordType(TsKeywordType {
+                    span: Default::default(),
+                    kind: TsKeywordTypeKind::TsNumberKeyword,
+                })),
+            }),
+            Some("boolean") => Ok(TsTypeAnn {
+                span: Default::default(),
+                type_ann: Box::new(TsType::TsKeywordType(TsKeywordType {
+                    span: Default::default(),
+                    kind: TsKeywordTypeKind::TsBooleanKeyword,
+                })),
+            }),
+            Some("json") => Ok(TsTypeAnn {
+                span: Default::default(),
+                type_ann: Box::new(TsType::TsKeywordType(TsKeywordType {
+                    span: Default::default(),
+                    kind: TsKeywordTypeKind::TsAnyKeyword,
+                })),
+            }),
+            Some(x) => parse_type_hint(x).map(|ty| TsTypeAnn {
+                span: Default::default(),
+                type_ann: Box::new(ty),
+            }),
+            None => continue,
+        };
 
+        if type_annotation.is_err() {
+            return Err(GenDeclForModelError::SDTypeHintParseError);
+        }
+
+        if attribute_type.is_some() {
+            elements.push(TsTypeElement::TsPropertySignature(TsPropertySignature {
+                span: Default::default(),
+                readonly: true,
+                key: Box::new(Expr::Lit(Lit::Str(Str {
+                    span: Default::default(),
+                    value: _attr_key_ident.sym.as_str().into(),
+                    raw: None,
+                }))),
+                computed: true,
+                optional: !attribute_required,
+                type_ann: Some(Box::new(type_annotation.unwrap())),
+            }));
         }
     }
 
-    if !_found_valid_model {
-        return Err(GenDeclForModelError::InvalidModel);
-    }
-    
-    Ok(TsInterfaceDecl { 
+    Ok(TsInterfaceDecl {
         span: Default::default(),
         id: Ident {
             span: Default::default(),
@@ -326,6 +214,39 @@ pub fn gen_decl_for_model(code: String, model_name: String) -> Result<TsInterfac
         body: TsInterfaceBody {
             span: Default::default(),
             body: elements,
-        }
+        },
     })
+}
+
+fn get_prop_as_str(prop: &PropName) -> Option<&str> {
+    match prop {
+        PropName::Ident(ident) => Some(ident.sym.as_str()),
+        PropName::Str(string) => string.value.as_str(),
+        _ => None,
+    }
+}
+
+fn parse_type_hint(type_hint: &str) -> Result<TsType, ()> {
+    let temp_type = format!("0 as {}", type_hint);
+    let cm: Lrc<SourceMap> = Default::default();
+    let type_file = cm.new_source_file(FileName::Anon.into(), temp_type);
+
+    let type_lexer = Lexer::new(
+        Syntax::Typescript(Default::default()),
+        Default::default(),
+        StringInput::from(&*type_file),
+        None,
+    );
+
+    let mut type_parser = Parser::new_from(type_lexer);
+
+    if let Ok(expr) = type_parser.parse_expr() {
+        if let Some(_ts_as) = expr.as_ts_as() {
+            Ok(*_ts_as.type_ann.clone())
+        } else {
+            Err(())
+        }
+    } else {
+        Err(())
+    }
 }
